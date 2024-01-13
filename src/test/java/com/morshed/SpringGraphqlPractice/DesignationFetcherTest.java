96c53498025e82bd4a99b90eaabb94869ce9fe28
Monjur-E-Morshed <monjurmorshed793@gmail.com>
2024-01-14 00:04:22 +0600

    missing imports removed.

--- src/test/java/com/morshed/SpringGraphqlPractice/DesignationFetcherTest.java
+++ src/test/java/com/morshed/SpringGraphqlPractice/DesignationFetcherTest.java
@@ -16,7 +16,6 @@ import org.springframework.util.Assert;
 import java.util.LinkedHashMap;
 import java.util.List;
 
-import static org.junit.jupiter.params.shadow.com.univocity.parsers.conversions.Conversions.notNull;
 
 @SpringBootTest(classes = {DgsAutoConfiguration.class, DesignationFetcher.class})
 public class DesignationFetcherTest {
