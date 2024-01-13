fd7e6b0ce1ab70863bf084a2c02c6176c331aaad
Monjur-E-Morshed <monjurmorshed793@gmail.com>
2024-01-13 20:16:04 +0600

    initial project set up

---
+++ src/test/java/com/morshed/SpringGraphqlPractice/SpringGraphqlPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morshed.SpringGraphqlPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphqlPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
